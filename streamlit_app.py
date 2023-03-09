920b5fa2f2c1ad12859d2c9994fd1034c0b049f2
anikale97 <127396371+anikale97@users.noreply.github.com>
2023-03-09 17:55:22 +0530

    Update streamlit_app.py
    Added Pandas and Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas
 streamlit.title('Diner')
 streamlit.header('Breakfast')
 streamlit.text('🍞 Vadapav')
 streamlit.text('🥣 Uttapam')
 streamlit.text('🥣 Dosa')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
